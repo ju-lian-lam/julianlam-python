89259c2690b8659574591b3d32dee593a2b23e9d
ju-lian-lam <lamj@dulwich.org.uk>
2025-09-08 12:15:07 +0100

    Create menu.py

---
+++ Programming Basics/menu.py
@@ -0,0 +1,8 @@
+choice = input("Enter a number: ")
+match choice:
+    case "1": print("You chose 1")
+    case "2": print("You chose 2")
+    case "3": print("You chose 3")
+    case "4": print("You chose 4")
+    case "5": print("You chose 5")
+    case _: print("Incorrect")
