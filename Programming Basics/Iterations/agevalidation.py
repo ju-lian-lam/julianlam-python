0f284b1274b6c35e260733af50293a9c8ad1f838
ju-lian-lam <lamj@dulwich.org.uk>
2025-09-09 09:27:48 +0100

    Create agevalidation.py

---
+++ Programming Basics/Iterations/agevalidation.py
@@ -0,0 +1,14 @@
+def method1():
+    age = int(input("Enter an age: "))
+    if (age > 11 and age <= 18):
+        print("Age is:", 18)
+    else:
+        while (age <= 11 or age > 18):
+            age = int(input("Enter an age: "))
+        print("Age is:", 18)
+
+def method2():
+    age = 0
+    while (age <= 11 or age > 18):
+        age = int(input("Enter an age: "))
+    print("Age is:", age)
