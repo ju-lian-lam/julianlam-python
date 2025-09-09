3a9be552cd4432440ee9b5494b0352953365f92a
ju-lian-lam <lamj@dulwich.org.uk>
2025-09-09 08:59:50 +0100

    Update menu.py

--- Programming Basics/menu.py
+++ Programming Basics/menu.py
@@ -2,7 +2,8 @@ choice = input("Enter a number: ")
 match choice:
     case "1": print("You chose 1")
     case "2": print("You chose 2")
-    case "3": print("You chose 3")
-    case "4": print("You chose 4")
+    case "A": print("You chose A")
+    case "B": print("You chose B")
     case "5": print("You chose 5")
     case _: print("Incorrect")
+# end match
