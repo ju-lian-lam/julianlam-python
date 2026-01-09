671ca0d428d5214915eafcdb5202418183b561b7
ju-lian-lam <lamj@dulwich.org.uk>
2026-01-09 22:25:48 +0800

    Create vpmain.py

---
+++ Programming Basics/Pygame/Vampire Game/vpmain.py
@@ -0,0 +1,39 @@
+from vpsettings import *
+from vpplayer import Player
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Survivor")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((400,300), self.all_sprites)
+
+    def run(self):
+        while self.running:
+            # dt
+            dt = self.clock.tick() / 1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # update
+            self.all_sprites.update(dt)
+
+            # draw
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
